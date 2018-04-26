75cf5b7652ad5437c0b247158a3509aefac7a5c4
Fakhri Fauzan <fazan697@gmail.com>
2018-04-26 07:32:01 +0700

    Convert to OO

--- QLearn.py
+++ QLearn.py
@@ -1,22 +1,50 @@
 import numpy as np
 import random
 
-load_reward = np.loadtxt("DataTugasML3.txt", dtype='i', delimiter='\t')
-reward = load_reward[::-1]
-q = np.zeros((10,10), dtype=int)
-gamma = 0.8
+class QLearning :
+    def __init__(self, gamma, load_reward):
+        self.gamma = gamma
+        self.load_reward = load_reward
+        self.reward = np.zeros((10,10), dtype=int)
+        self.q = np.zeros((10,10), dtype=int)
 
-current = random.randint(0,9)
+    def initialize_reward(self):
+        load_reward = np.loadtxt(self.load_reward, dtype='i', delimiter='\t')
+        self.reward = load_reward[::-1]
 
-def next_action(current) :
-    next = []
-    for i in range(reward[current].size):
-        if(reward[current, i] != -1) :
-            next.append(i)
-    return random.choice(next)
+    def initialize_q(self):
+        self.q = np.zeros((10,10), dtype=int)
 
-print(reward)
-print(current)
-print(next_action(current))
+    def next_action(self, state):
+        next = []
+        for i in range(self.reward[state].size):
+            if (self.reward[state,i] != -1):
+                next.append(i)
+        return random.choice(next)
+
+    def max_q(self, next):
+        list = []
+        value = []
+        for i in range(self.reward[next].size):
+            if (self.reward[next, i] != -1):
+                list.append(i)
+        for j in range(len(list)):
+            value.append(list[j])
+        return max(value)
+
+    def update_q(self, state, action):
+        self.q[state, action] = self.reward[state, action] + self.gamma * self.max_q(action)
+
+    def main(self):
+        current = random.randint(0, 9)
+        print(current)
+
+
+# MAIN PROGRAM #
+file_reward = "DataTugasML3.txt"
+qlearn = QLearning(0.8, file_reward)
+qlearn.initialize_reward()
+qlearn.initialize_q()
+qlearn.main()
 
 # print(reward[::-1])
